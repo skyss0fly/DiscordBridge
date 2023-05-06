c730fd45e001f3bef0bb535258b7f838efb562b5
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-05-06 11:41:52 +1000

    Update main.php

--- src/skyss0fly/DiscordBridge/main.php
+++ src/skyss0fly/DiscordBridge/main.php
@@ -3,11 +3,11 @@
 namespace skyss0fly/DiscordBridge;
 use pocketmine\plugin\PluginBase;
 use pocketmine\plugin\PluginManager;
-
+use Resources;
 class Main extends PluginBase {
 public function OnLoad(): void{
 $this->getLogger()->info('Loading DiscordBridge')
-  if $this->getConfig( RESOURCES.CONFIG.YML .. $DiscordBotKey) == nil:
+  if $this->getConfig( RESOURCES:CONFIG.YML .. $DiscordBotKey = $this->getConfig(DiscordBotKey)) == nil:
   $this->getLogger()->Error('Your Bot Key is invalid, disabling plugin')
     	$this->pluginManager->disablePlugin()
   }
